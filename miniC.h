b853a2543d4f7a1fae74d4193efd062b9bd6ddcb
endof603 <maksmelnuk99@gmail.com>
2025-06-29 16:55:12 +0300

    Add files via upload

---
+++ miniC.h
@@ -0,0 +1,15 @@
+#ifndef DEF_H
+#define DEF_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define pf(text) printf(text)
+#define te true
+#define fe false
+#define dk break
+#define rn(n) return n
+#define we(condition){} while(condition){}
+#define sp(time) sleep(time)
+
+#endif
